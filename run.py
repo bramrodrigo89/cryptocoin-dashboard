9becc424f7e4eb625a5b328403d814736efec17f
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-05-12 20:56:45 +0000

    depoyment to Heroku

--- run.py
+++ run.py
@@ -7,15 +7,17 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'cryptocoins_db'
-
 app.config['MONGO_URI']=os.getenv("MONGO_URI")
 
-mongo= PyMongo(app)
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/home')
 def say_hello():
-    return render_template("index.html")
+    print(mongo.db.users.find())
+    return render_template("index.html", users=mongo.db.users.find())
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=os.getenv("IP","0.0.0.0"),
+        port=int(os.getenv("PORT","5000")),
+        debug=True)
