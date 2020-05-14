302fd8ab95fb8d246207215e0a420271740b0a63
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-05-14 21:04:37 +0000

    added Materialize for making styles, created list of symbols for extracting information from bitcoin prices

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import globalVal
 if os.path.exists('env.py'):
     import env
 from flask import Flask, render_template, redirect, request, url_for, jsonify
@@ -13,13 +14,9 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/home')
 def say_hello():
-    return render_template("index.html", users=mongo.db.users.find())
-
-@app.route('/stock/<symbol>')
-def get_quote(symbol):
-    stock = Stock(symbol)
-    quote_data= stock.get_quote()
-    return render_template("quote.html", quote=quote_data)
+    batch = Stock(os.getenv['CRYPTO_SYMBOLS'])
+    quote_batch_data= batch.get_quote()
+    return render_template("crypto.html", users=mongo.db.users.find(), data=quote_batch_data)
 
 @app.route('/crypto/<symbol>')
 def get_crypto_quote(symbol):
