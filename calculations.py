8f63a92af3b692605ef3e84a63243a6a06427050
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-05-20 03:32:52 +0000

    added function to calculate total ticker for every coin in user wallet

--- calculations.py
+++ calculations.py
@@ -1,12 +1,13 @@
 
 def calculate_balance(wallet_object,available_cash):
-    for cryptocoin in wallet_object:
+    for coin,obj in wallet_object.items():
         total_ticker=0
         total_balance_toAdd=0
-        for transaction in cryptocoin:
-            print(transaction.items())
-            total_ticker+=transaction.ticker
-            total_balance_toAdd+=transaction.ticker*transaction.price
-        print('Total ticker Crypto: '+total_ticker)
-        print('Total balance to add for this crypto: '+ total_balance_toAdd)
-        print('Available cash: '+available_cash)
+        for date,transaction in obj.items():
+            total_ticker+=transaction['ticker']
+            total_balance_toAdd+=transaction['ticker']*transaction['price']
+            print('Now adding ',total_balance_toAdd)
+            #total_ticker+=transaction.ticker
+            #total_balance_toAdd+=transaction.ticker*transaction.price
+        print('Total ticker for ',coin,'is ',total_ticker)
+        print('Total balance to add for ',coin,'is ',total_balance_toAdd)
