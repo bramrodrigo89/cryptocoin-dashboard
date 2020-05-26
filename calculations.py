8c164829d4d8ce65e556ce2af191969dbe3339be
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-05-26 21:41:23 +0000

    added descriptions for all functions in calculations.py, improved code in run.py and started route for removing elements from favorites list but not finished

--- calculations.py
+++ calculations.py
@@ -14,10 +14,12 @@ def spent_cash(ticker,price):
 
 def updated_price_coins(wallet_object):
     """
-    This function returns a small dictionary of the total value of every cryptocoin
+    This function returns a a dict {'symbol':value} of the total value of every cryptocoin
     in the user's wallet, using the formula:
 
     Total Value = Latest Price * Total Ticker in Wallet
+
+    This value is used later to calculate the total balance in wallet
 
     """
     wallet_coins=wallet_object['coins']
@@ -29,6 +31,16 @@ def updated_price_coins(wallet_object):
     return updated_price_wallet_coins
 
 def value_change_coins(wallet_object):
+    """
+    This function returns a dict {'symbol':value} of the value change for every cryptocoin
+    in the user's wallet, using the formula:
+
+    Value Change = (Latest Price * Total Ticker) - Previous Prices * Transaction Tickers
+
+    So that all the previous transactions on that specific asset are considered. These 
+    values ared used later to calculate the value change in the total balance.
+
+    """
     wallet_coins=wallet_object['coins']
     value_change_coins={}
     for symbol,obj in wallet_coins.items():
@@ -41,23 +53,41 @@ def value_change_coins(wallet_object):
         value_change_coins[symbol]=value_change
     return value_change_coins
 
-def calculate_balance_and_change(wallet_object,available_cash):
-    updated_price_coins_list=updated_price_coins(wallet_object)
-    value_change_coins_list=value_change_coins(wallet_object)
+def balance_prices_and_changes(wallet_object,available_cash):
+    """
+    This function returns an object with information about the in the user's wallet. 
+    The returned values of the previous functions are added up and returned here as
+    Balance_Object = { 'total': value, 'change':value, 'percent':value }
+
+    The objects returned from the two functions defined before are returned again to be
+    passed into the following calculationsf, in order to avoid calling Stock.get_quote() 
+    which produces slightly different cryptocoin prices every second it is called. 
+
+    """
+    updated_price_coins_obj=updated_price_coins(wallet_object)
+    value_change_coins_obj=value_change_coins(wallet_object)
     total_balance=float(available_cash)
     total_value_change_coins=0
     balance_and_change_object={}
-    for symbol,added_value in updated_price_coins_list.items():
+    for symbol,added_value in updated_price_coins_obj.items():
         total_balance+=added_value
     balance_and_change_object['total']=total_balance
-    for symbol,change_value in value_change_coins_list.items():
+    for symbol,change_value in value_change_coins_obj.items():
         total_value_change_coins+=change_value
     balance_and_change_object['change']=total_value_change_coins
     balance_and_change_object['percentChange']=100*total_value_change_coins/total_balance
-    return balance_and_change_object, updated_price_coins_list, value_change_coins_list
+    return balance_and_change_object, updated_price_coins_obj, value_change_coins_obj
 
-def fetch_wallet_coins_data(updated_price_list, value_change_list, wallet_object,db_cryptocoin_obj):
-    value_changes_list=value_change_list
+def fetch_wallet_coins_data(updated_price_obj, value_change_obj, wallet_object, db_cryptocoin_obj):
+    """
+    This function produces the object used for "My Crypto Wallet" section. The same updated
+    prices and value changes from the previous functions are inserted here again to produce
+    a new dict with this content for every coin in the user's wallet. :
+
+    {'symbol':{'name':str, 'symbol_short':str, 'balance':value, 'value_change':value, 
+    'change_percent':value, 'total_ticker':value }}
+
+    """
     wallet_coins=wallet_object['coins']
     wallet_coins_list=[]
     for symbol,obj in wallet_coins.items():
@@ -69,10 +99,10 @@ def fetch_wallet_coins_data(updated_price_list, value_change_list, wallet_object
             if coin_symbol == elem['symbol_long']:
                 coin_info['name'] = elem['name']
                 coin_info['symbol_short'] = elem['symbol_short']
-        for symbol,balance in updated_price_list.items():
+        for symbol,balance in updated_price_obj.items():
             if coin_symbol == symbol:
                 coin_info['balance']= balance
-        for symbol,change in value_change_list.items():
+        for symbol,change in value_change_obj.items():
             if coin_symbol == symbol:
                 coin_info['value_change']= change
                 coin_info['value_change_percent']=100*change/coin_info['balance']
@@ -81,12 +111,18 @@ def fetch_wallet_coins_data(updated_price_list, value_change_list, wallet_object
                 coin_info['total_ticker']=obj['total_ticker']
     return wallet_coins_object
 
-def create_plot(updated_price_list,user_object):
+def create_plot(updated_price_obj,user_object):
+    """
+    This function produces a JSON object to create a pie diagram of the wallet's
+    coins distribution and calculates proportions. It also consideres the available cash
+    as the first element in the list. 
+
+    """
     cash_value=user_object['cash']
     user_wallet=user_object['wallet']
     pie_labels=['Available Cash']
     pie_values=[cash_value]
-    for coin,value in updated_price_list.items():
+    for coin,value in updated_price_obj.items():
         pie_labels.append(coin)
         pie_values.append(float(value))
     data_pie=[go.Pie(labels=pie_labels, values=pie_values, hole=.3)]
@@ -94,18 +130,18 @@ def create_plot(updated_price_list,user_object):
     return graphJSON
 
 def favorite_list_data(user_object,wallet_coins_object,db_cryptocoin_obj):
-    favorite_list=user_object['favorites'].split(", ")
+    favorite_list=user_object['favorites'].split(",")
     favorite_list_data={}
     for favorite in favorite_list:
         for coin_symbol, coin_info in wallet_coins_object.items():
             if favorite == coin_symbol:
                 favorite_list_data[coin_symbol]=coin_info
-            elif favorite != coin_symbol:
-                new_quote_favorite=Stock(favorite)
-                quote_favorite_data=new_quote_favorite.get_quote()
-                favorite_list_data[favorite]=quote_favorite_data
-                for elem in db_cryptocoin_obj:
-                    if favorite == elem['symbol_long']:
-                        favorite_list_data[favorite]['name'] = elem['name']
-                        favorite_list_data[favorite]['symbol_short'] = elem['symbol_short']
+        if favorite not in wallet_coins_object.keys():
+            new_quote_favorite=Stock(favorite)
+            quote_favorite_data=new_quote_favorite.get_quote()
+            favorite_list_data[favorite]=quote_favorite_data
+            for elem in db_cryptocoin_obj:
+                if favorite == elem['symbol_long']:
+                    favorite_list_data[favorite]['name'] = elem['name']
+                    favorite_list_data[favorite]['symbol_short'] = elem['symbol_short']
     return favorite_list_data
--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from flask import Flask, render_template, redirect, request, url_for, jsonify
 from flask_pymongo import PyMongo
 from iexfinance.stocks import Stock, get_historical_data
-from calculations import updated_price_coins, value_change_coins, calculate_balance_and_change, create_plot, fetch_wallet_coins_data, favorite_list_data
+from calculations import updated_price_coins, value_change_coins, balance_prices_and_changes, create_plot, fetch_wallet_coins_data, favorite_list_data
 
 app = Flask(__name__)
 
@@ -24,18 +24,24 @@ for coin in CRYPTOCOIN_OBJECT:
     CRYPTOCOINS_LIST.append(coin)
 
 @app.route('/')
-@app.route('/user/<user>/dashboard')
-def show_user_dashboard():
-    user_data=mongo.db.users.find_one({'username':'bramrodrigo89'})
-    balance_tuple=calculate_balance_and_change(user_data['wallet'],user_data['cash'])
-    balance_data=balance_tuple[0]
-    updated_prices=balance_tuple[1]
-    updated_changes=balance_tuple[2]
+@app.route('/user/<username>/dashboard')
+def show_user_dashboard(username):
+    user_data=mongo.db.users.find_one({'username':username})
+    data=balance_prices_and_changes(user_data['wallet'],user_data['cash'])
+    balance_data, updated_prices, updated_changes = data[0], data[1], data[2]
     wallet_coins_data=fetch_wallet_coins_data(updated_prices, updated_changes, user_data['wallet'],CRYPTOCOINS_LIST)
     pie_data = create_plot(updated_prices,user_data)
     favorites_data=favorite_list_data(user_data,wallet_coins_data,CRYPTOCOINS_LIST)
     return render_template("dashboard.html", user=user_data, balance=balance_data, plot=pie_data, wallet_coins=wallet_coins_data ,favorites=favorites_data)
 
+@app.route('/remove-fav/<username>/<symbol>')
+def remove_favorite(username, symbol):
+    user_data=mongo.db.users.find_one({'username':username})
+    favorites_list=user_data['favorites'].split(",")
+    favorites_list.remove(symbol)
+    updated_favorites_list=(','.join(favorites_list))
+    mongo.db.users.update( {'username': username}, {'favorites':updated_favorites_list})
+    return redirect(url_for('show_user_dashboard',username=username))
 
 if __name__ == '__main__':
     app.run(host=os.getenv("IP","0.0.0.0"),
