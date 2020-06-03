f8fcc97a08df90a9c3261181207ebffdbe8c5d33
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-06-03 05:54:13 +0000

    added javascript functionality to disable buy button in buy-coin-modal in order to avoid users buying more than available cash in funds allows

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,4 +1,13 @@
+// Taken from "How to format a number as a currency value in JavaScript"
+// https://flaviocopes.com/how-to-format-number-as-currency-javascript/
 
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+})
+
+const buyButton = document.getElementById('modal-buy-button');
 
 $(document).on("click", ".open-buy-coin-modal-link", function () {
     var selectedCoinSymbol = $(this).attr('data-id');
@@ -20,6 +29,25 @@ $(document).on("click", ".open-buy-coin-modal-link", function () {
     $('#submit-buy-coin-bid-price').val(selectedCoinBidPrice);
 });
 
+function check_cash_left(cash_spent){
+    var submitBuyButton =  $('#modal-buy-button');
+    var available_cash = $('#modal-cash-available').html().split(" ").pop().replace(/,/g, "");
+    cash_left = available_cash - cash_spent
+    if (cash_left >= 0.0) {
+        buyButton.disabled = false;
+        submitBuyButton.removeClass('tooltipped');
+        submitBuyButton.removeAttr('data-position');
+        submitBuyButton.removeAttr('data-tooltip');
+        submitBuyButton.removeAttr('data-tooltip-id')
+    } else {
+        buyButton.disabled = true;
+        submitBuyButton.addClass('tooltipped');
+        submitBuyButton.attr('data-position', 'top');
+        submitBuyButton.attr('data-tooltip', 'You do not have enough cash in your wallet. Add more funds to keep buying coins!');
+        submitBuyButton.tooltip();
+    }
+}
+
 function updatePrice(){
     var bid_price = $('#modal-buy-coin-bid-price').html().split(" ").pop().replace(/,/g, "");
     var ticker = parseFloat($('#ticket-entry-number').val());
@@ -28,9 +56,22 @@ function updatePrice(){
     $('#cash-spent-entry').val(cash_spent);
     $('#cash-spent-entry').focus();
     $('#ticket-entry-number').focus();
+    check_cash_left(cash_spent);
+}
+
+function updateTicker(){
+    var bid_price = $('#modal-buy-coin-bid-price').html().split(" ").pop().replace(/,/g, "");
+    var cash_spent = parseFloat($('#cash-spent-entry').val());
+    var calculated_ticker = cash_spent/bid_price;
+    var calculated_ticker = calculated_ticker.toFixed(2);
+    $('#ticket-entry-number').val(calculated_ticker);
+    $('#ticket-entry-number').focus();
+    $('#cash-spent-entry').focus();
+    check_cash_left(cash_spent); 
 }
 
 $(document).on("change, keyup", "#ticket-entry-number", updatePrice);
+$(document).on("change, keyup", "#cash-spent-entry", updateTicker);
 
 
 
