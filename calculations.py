c51cced44cca8f5d7274595cefff77950b77a3fa
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-06-12 20:37:31 +0000

    working on function to create line chart for wallet's performance

--- calculations.py
+++ calculations.py
@@ -144,21 +144,58 @@ def create_pie_chart(updated_price_obj,user_object):
     graphJSON = json.dumps(data_pie, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
 
-def create_plot(updated_price_obj,user_object):
+def create_line_chart(user_object):
     """
     This function ...
 
     """
-    cash_value=user_object['cash']
     user_wallet=user_object['wallet']
-    pie_labels=['Available Cash']
-    pie_values=[cash_value]
+    wallet_coins=user_wallet['coins']
+    labels=[]
+    values=[]
     for coin,value in updated_price_obj.items():
-        pie_labels.append(coin)
-        pie_values.append(float(value))
-    data_pie=[go.Pie(labels=pie_labels, values=pie_values, hole=.3)]
-    graphJSON = json.dumps(data_pie, cls=plotly.utils.PlotlyJSONEncoder)
+        labels.append(coin)
+        values.append(float(value))
+    data_plot=[go.Pie(labels=pie_labels, values=pie_values, hole=.3)]
+    graphJSON = json.dumps(data_plot, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
+
+    #example
+
+    # Add data
+    # month = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+    #          'August', 'September', 'October', 'November', 'December']
+    # high_2000 = [32.5, 37.6, 49.9, 53.0, 69.1, 75.4, 76.5, 76.6, 70.7, 60.6, 45.1, 29.3]
+    # low_2000 = [13.8, 22.3, 32.5, 37.2, 49.9, 56.1, 57.7, 58.3, 51.2, 42.8, 31.6, 15.9]
+    # high_2007 = [36.5, 26.6, 43.6, 52.3, 71.5, 81.4, 80.5, 82.2, 76.0, 67.3, 46.1, 35.0]
+    # low_2007 = [23.6, 14.0, 27.0, 36.8, 47.6, 57.7, 58.9, 61.2, 53.3, 48.5, 31.0, 23.6]
+    # high_2014 = [28.8, 28.5, 37.0, 56.8, 69.7, 79.7, 78.5, 77.8, 74.1, 62.6, 45.3, 39.9]
+    # low_2014 = [12.7, 14.3, 18.6, 35.5, 49.9, 58.0, 60.0, 58.6, 51.7, 45.2, 32.2, 29.1]
+
+    # fig = go.Figure()
+    # # Create and style traces
+    # fig.add_trace(go.Scatter(x=month, y=high_2014, name='High 2014',
+    #                          line=dict(color='firebrick', width=4)))
+    # fig.add_trace(go.Scatter(x=month, y=low_2014, name = 'Low 2014',
+    #                          line=dict(color='royalblue', width=4)))
+    # fig.add_trace(go.Scatter(x=month, y=high_2007, name='High 2007',
+    #                          line=dict(color='firebrick', width=4,
+    #                               dash='dash') # dash options include 'dash', 'dot', and 'dashdot'
+    # ))
+    # fig.add_trace(go.Scatter(x=month, y=low_2007, name='Low 2007',
+    #                          line = dict(color='royalblue', width=4, dash='dash')))
+    # fig.add_trace(go.Scatter(x=month, y=high_2000, name='High 2000',
+    #                          line = dict(color='firebrick', width=4, dash='dot')))
+    # fig.add_trace(go.Scatter(x=month, y=low_2000, name='Low 2000',
+    #                          line=dict(color='royalblue', width=4, dash='dot')))
+
+    # # Edit the layout
+    # fig.update_layout(title='Average High and Low Temperatures in New York',
+    #                    xaxis_title='Month',
+    #                    yaxis_title='Temperature (degrees F)')
+
+
+    # fig.show()
 
 def favorite_list_data(user_object,wallet_coins_object,db_cryptocoin_obj):
     """
